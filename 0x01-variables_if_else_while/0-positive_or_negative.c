ab89f180c280b8440d604ebed438eaad2a60fd95
rhema254 <rhemambaabu@gmail.com>
2023-02-17 05:25:03 -0800

    Positive or Negative Test Script

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,10 +2,10 @@
 #include <time.h>
 #include <stdio.h>
 /**
- * main - prints whether a variable stored in is either positive or negative or zero
+ * main - prints whether n is either positive or negative or zero
  *
  *Return: 0 (Success)
- * */
+ */
 
 /* betty style doc for function main goes there */
 int main(void)
@@ -15,15 +15,15 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	if (n>0)
+	if (n > 0)
 	{
 		printf("%d is positive\n", n);
 	}
-	else if (n==0)
+	else if (n == 0)
 	{
 		printf("%d is zero\n", n);
 	}
-	else 
+	else
 	{
 		printf("%d is negative\n", n);
 	}
