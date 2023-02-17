e6bc73d259ac3443c289083b722feb9b88c7b153
rhema254 <rhemambaabu@gmail.com>
2023-02-17 15:49:26 -0800

    Print Numbers Script

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints out single digit numbers 
+ * single digit number start from 0
+ * Retuen: 0 (Success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+	{
+		printf("%d", a);
+	}
+	printf("\n");
+	return (0);
+}
