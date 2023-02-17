f6afc017fcd0abe42818b5da29ba36dffae0c13b
rhema254 <rhemambaabu@gmail.com>
2023-02-17 14:54:23 -0800

    Print Alphabets Script

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/** 
+ * main - prints out an alphabet 
+ * followed by a new line
+ * Return: 0 (Success)
+ */ 
+int main(void)
+{
+	char AZ;
+
+	for(AZ = 'a'; AZ <= 'z'; AZ++)
+	{
+		putchar(AZ);
+		putchar('\n');
+	}
+	return 0;
+}
