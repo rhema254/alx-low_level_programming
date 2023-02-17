10b5d261723d6afd7344a5a6f71ac2c4015a40a7
rhema254 <rhemambaabu@gmail.com>
2023-02-16 18:53:58 -0800

    Size Script

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - A program that prints out size various computer types
+ * Return: 0 (Success)
+ */
+int main(void)
+{ 
+	int a;
+	char b;
+	long int c;
+	long long int d;
+	float f;
+
+	printf("Size of a char:%lubyte(s)\n",(unsigned long)sizeof(a));
+	printf("Size of an int:%lubyte(s)\n", (unsigned long)size of(b))
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+	return (0);
+}
