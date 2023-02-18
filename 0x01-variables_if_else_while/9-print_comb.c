099a58f3e90a5d03a239986b1d5ec01dc86c9d59
rhema254 <rhemambaabu@gmail.com>
2023-02-17 16:50:35 -0800

    Print no combinations

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
+/**
+ * main - Prints all posible combinations of single number digits
+ * followed by a newline
+ * Return: 0 (Success)
+ */
 
-int main() {
-    int n;
+int main(void)
+{
+	int n;
 
 	for (n = 48; n < 58; n++)
 	{
