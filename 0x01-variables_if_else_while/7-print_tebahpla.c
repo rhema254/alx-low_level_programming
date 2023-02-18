cf8f75b696ee2a7023c51c37a1e53b6c38bae4b2
rhema254 <rhemambaabu@gmail.com>
2023-02-17 16:14:32 -0800

    Reverse alphabet Script

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - prints lower case alphabet in reverse
+ * followed by a newline
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char az;
+
+	for (az = 'z'; az >= 'a';az--)
+	{
+		putchar(az);
+	}
+	putchar('\n');
+	return (0);
+}
