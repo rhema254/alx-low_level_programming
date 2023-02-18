cb425e52cec1c1abca424cdc5f8b0eb6639371d9
rhema254 <rhemambaabu@gmail.com>
2023-02-17 16:33:03 -0800

    Hex numbers Script

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - prints out numbers of base 16 in lowercase
+ * followed by a newline
+ * Return:0 (Success)
+ */
+int main(void)
+{
+	char az;
+	int a;
+	
+	for (a = 48; a < 58; a++)
+	{
+		putchar(a);
+	}
+	for (az = 97; az <= 102; az++)
+	{
+		putchar(az);
+	}
+	putchar('\n');
+	return (0);
+}
