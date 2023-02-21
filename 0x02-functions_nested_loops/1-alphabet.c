0b4973bbd93c1cb85f186ff2fe9a811d8c3c7164
rhema254 <rhemambaabu@gmail.com>
2023-02-21 13:42:14 -0800

    1-alphabet script

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints out alphabets in lower case
+ * followed by a newline
+ * Return: 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+	char az;
+
+	for (az = 'a', az <= 'z', az++)
+	{
+		_putchar(ch);
+		_putchar('\n');
+	}
+}
