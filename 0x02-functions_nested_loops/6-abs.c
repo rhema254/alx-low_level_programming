3cb5bd2d18650e99f8d185ca7036eddf6a9efd33
rhema254 <rhemambaabu@gmail.com>
2023-02-21 15:31:13 -0800

    Absolute Value Script

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _abs - Prints the absolute value of any integer
+ * @n: the integer to check
+ * Return: the absolute value of int
+ */
+
+int _abs(int)
+{
+	int n;
+
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-1);
+	}
+}
