f0f8d7ad15683b7009b51f933b5a9ab5f64e2360
rhema254 <rhemambaabu@gmail.com>
2023-02-21 14:53:50 -0800

    Alphabet Checker Script

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks whether int c is an alphabet
+ * @c: is the int being checked
+ * Return: 1 if c is an alphabet, otherwise 0
+ */
+
+int _isalpha(int c)
+{
+	if((c >= 'a' && c <= 'z') && (c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
