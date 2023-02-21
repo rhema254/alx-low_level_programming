8c4eda7d7c8c5841dcf1ce2920518058bf85787a
rhema254 <rhemambaabu@gmail.com>
2023-02-21 15:56:56 -0800

    last digit extractor

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Prints the last digit of any integer
+ * @n: the integer to get the last digit from
+ * Return: last value of a number
+ */
+
+int print_last_digit(int n)
+{
+	int a = n % 10;
+
+	return (a);
+}
