a56d309cbda4269e82d3e3ce7f994c45894fe62e
rhema254 <rhemambaabu@gmail.com>
2023-02-21 14:06:38 -0800

    Printing Alphabets 10 times SScript

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/** 
+ * print_alphabet_x10 - prints the alphabet in lowercase 10 times
+ * followed by a newline
+ */
+
+void print_alphabet_x10(void)
+{
+	int a;
+	char az;
+
+	do	
+	{
+		for (az = 'a'; az <= 'z'; az++)
+		{
+			_putchar(az);
+		}
+		_putchar('\n');
+	}
+	while (a < 9)
+}
