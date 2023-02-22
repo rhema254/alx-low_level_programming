48f6ca6c79a308dbd0533997564a2b7abf6d6d2e
rhema254 <rhemambaabu@gmail.com>
2023-02-21 17:23:07 -0800

    Addition Script

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,11 +2,13 @@
 
 /**
  * add - It sums any two integers given in the function
- * Returns the sum of the integers
+ * @a: the first operand in the operation
+ * @b: the second operand in the operation
+ * Return: the sum of the integers
  */
 
 int add(int a, int b)
-{
+{	
 	int sum = a + b;
 	
 	return (sum);
