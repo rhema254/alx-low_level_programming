c1a58b7ed3517cf6e9f76e252f802a58f84cbd62
rhema254 <rhemambaabu@gmail.com>
2023-02-21 17:19:38 -0800

    Addition Script

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - It sums any two integers given in the function
+ * Returns the sum of the integers
+ */
+
+int add(int a, int b)
+{
+	int sum = a + b;
+	
+	return (sum);
+}
