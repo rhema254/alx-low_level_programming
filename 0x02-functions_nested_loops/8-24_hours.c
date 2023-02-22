f04d796be48860908250b0ccb62a9d66269db0ab
rhema254 <rhemambaabu@gmail.com>
2023-02-21 16:49:42 -0800

    8-24 hours Script

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,9 +8,9 @@
 void jack_bauer(void)
 {
 	int x = 0;
-	int a = 0,b = 0,c = 0,d = 0;
+	int a = 0, b = 0, c = 0, d = 0;
 
-	while (x < 1440 )
+	while (x < 1440)
 	{
 		_putchar(a + '0');
 		_putchar(b + '0');
