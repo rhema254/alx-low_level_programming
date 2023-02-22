f8f9087fcafa6acfc8f8907319521e11fa9dfff4
rhema254 <rhemambaabu@gmail.com>
2023-02-21 17:08:39 -0800

    Multiplication table Script

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * times_table - prints the multiplication table starting from 9
+ */
+
+void times_table(void)
+{
+	int a, b, c, d, e;
+	
+	for (a = 0; a < 10; a++)
+	{
+	for (b = 0; b < 10; b++)
+	{
+		c = a * b;
+		
+		if (c < 10 && j != 0)
+		{
+			_putchar(44);
+			_putchar(32);
+			_putchar(32);
+		}
+		if (c < 10)
+			_putchar('0' + c);
+
+		if (c > 9)
+		{
+			d = c / 10;
+			e = c % 10;
+
+			_putchar(44);
+			_putchar(32);
+			_putchar('0' + d);
+			_putchar('0' + m);
+		}
+	}
+	_putchar('\n');
+	}
+}
