cbf5a5e3f5a3e9727c535ec0b3d38cd01e76742c
rhema254 <rhemambaabu@gmail.com>
2023-02-24 04:31:57 +0300

    Integer Multiplier

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - multilplies two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: product
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
