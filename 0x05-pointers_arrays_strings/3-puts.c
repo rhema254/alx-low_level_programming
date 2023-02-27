9f0f8ee8e44df095561ada1a5ed6e76aa9325f48
rhema254 <rhemambaabu@gmail.com>
2023-02-27 06:53:54 -0800

    Fear Lack of Computers

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _puts - prints a string
+ * followed by a newline
+ * @str: the string
+ * Return: 0
+ */
+
+void _puts(char *str)
+{
+	int a;
+
+	a = 0;
+
+	while (str[a] != '\0')
+	{
+		puthcar(a);
+		a++;
+	}
+	putchar('\n');
+}
