7b9cd2dcfb9734dbd371d4c7d14ce3dafa443c71
rhema254 <rhemambaabu@gmail.com>
2023-03-01 08:41:28 -0800

    Update Repo

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,5 +1,5 @@
-#ifndef "MAIN_H"
-#define "MAIN_H"
+#ifndef MAIN_H
+#define MAIN_H
 
 #include <stdio.h>
 
@@ -17,4 +17,4 @@ void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
 
-#endif 
+#endif
