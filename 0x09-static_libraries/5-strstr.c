df4c8785331b61ac19a612fde495a8228f2588b6
rhema254 <rhemambaabu@gmail.com>
2023-03-10 15:39:17 -0800

    Update

--- 0x09-static_libraries/5-strstr.c
+++ 0x09-static_libraries/5-strstr.c
@@ -22,28 +22,4 @@ char *_strstr(char *haystack, char *needle)
 			return (&haystack[i]);
 	}
 	return (NULL);
-#include "main.h"
-#include <stdio.h>
-/**
- * *_strstr - locates a substring
- * @haystack: string to search in
- * @needle: substring to look for *
- * Return: pointer to the beginning of the located substring
- * NULL if not found
- */
-char *_strstr(char *haystack, char *needle)
-{
-	int i, j;
-
-	for (i = 0; haystack[i] != '\0'; i++)
-	{
-		for (j = 0; needle[j] != '\0'; j++)
-		{
-			if (haystack[i + j] != needle[j])
-				break;
-		}
-		if (!needle[j])
-			return (&haystack[i]);
-	}
-	return (NULL);
-}}
+}
