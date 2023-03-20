84f96edbfa60fb9667d29fdac642ea740322d011
rhema254 <rhemambaabu@gmail.com>
2023-03-20 08:16:43 -0700

    4. A door is what a dog is perpetually on the wrong side of

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,65 @@
+#include "dog.h"
+/**
+ * length - Measure the length of a string
+ * @string: The string to be measured
+ * Return: The length of the string
+ */
+
+int length(char *string)
+{
+	int i;
+
+	for (i = 0; *(string + i); i++)
+		;
+	return (i);
+}
+/**
+ * copy_S - Copy a string
+ * @original: The original string
+ * @copy: The copy
+ */
+
+void copy_S(char *original, char *copy)
+{
+	int i;
+
+	for (i = 0; i <= length(original); i++)
+	{
+		*(copy + i) = *(original + i);
+	}
+}
+
+/**
+ * new_dog - Create a new dog with given parameters
+ * @name: The name of the dog
+ * @age: The age of the dog
+ * @owner: the owner of the dog
+ *
+ * Return: the created dog
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dog;
+
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
+		return (NULL);
+
+	dog->name = malloc(length(name) + 1);
+	if (dog->name == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
+	dog->age = age;
+	dog->owner = malloc(length(owner) + 1);
+	if (dog->owner == NULL)
+	{
+		free(dog);
+		free(dog->name);
+		return (NULL);
+	}
+	copy_S(name, dog->name);
+	copy_S(owner, dog->owner);
+	return (dog);
+}
