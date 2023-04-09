9edc1481007c3139f85fe62735aa49077c5d9f7a
rhema254 <rhemambaabu@gmail.com>
2023-04-09 11:30:21 -0700

    Set Binary Bit

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * set_bit - Fn that sets the value of a bit to 1 at a given index
+ * @n: integer in the parameter
+ * @index: is the index starting from 0 of the bit you want to set
+ * Return: 1 if success otherwise -1
+ */
+
+int set_bit(unsigned long int *n, unsingned int index)
+{
+	unsigned int ind;
+
+	if (index > 63)
+		return (-1);
+
+	ind = 1 << index;
+	*n = (ind | *n);
+	return (1);
+}
