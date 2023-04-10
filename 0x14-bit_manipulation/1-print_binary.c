af883e8c8cb01ce7ce42e941f8b09217f276c46a
rhema254 <rhemambaabu@gmail.com>
2023-04-10 04:14:25 -0700

    Update 2

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,8 +8,22 @@
 
 void print_binary(unsigned long int n)
 {
-	if (n > 1);
+	int i;
+	int count = 0;
+	unsigned long int current;
 
-	print_binary(n >> 1);
-	_putchar((n & 1) + '0');
+	for (i = 63; i >= 0; i--)
+	{
+		current = n >> 1;
+
+		if (current & 1)
+			_putchar('1');
+			count ++;
+
+		else if (count)
+		_putchar('0');
+	}
+	if (!count)
+	_putchar('0');
 }
+
