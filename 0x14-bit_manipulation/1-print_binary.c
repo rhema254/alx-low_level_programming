30bc453fb2697d885452124c2c660d1268b95acd
rhema254 <rhemambaabu@gmail.com>
2023-04-10 04:40:42 -0700

    Print Binary Trial 4

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_binary - Function that gives the binary equivalent of a number
+ * @n: The number to be converted
+ * Return: The converted number
+ */
+
+void print_binary(unsigned long int n)
+{
+	if (n > 1)
+		print_binary(n >> 1);
+	_putchar((n & 1) + '0');
+}
